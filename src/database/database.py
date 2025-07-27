e165f032a4f8d22606bfde317d89f7f5676acc19
Generatorcc <133531334+Generatorcc@users.noreply.github.com>
2025-07-27 13:45:49 -0700

    Update database.py

--- src/database/database.py
+++ src/database/database.py
@@ -1,12 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# In a real application, get this from environment variables
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/project_manager_db"
-
-enfine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=enfine)
-
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/ghq_db"
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
-
